28193eef87c8e61fb135d415e21af004e7013974
Nura Nurbekova <nura.nurbekova.kk@gmail.com>
2021-09-23 23:20:31 +0600

    added 4-5 tasks

---
+++ JS-4-vars/script.js
@@ -0,0 +1,15 @@
+                   //------1--------
+prompt("What's your name?");
+var myName = 'Nurbek'; //var -Keyword; myName - Name; Nura - value;
+myName = 'Nura'; //удалит предыдущий и переназначает новое значение. В этом случае не нужно сново писать слово var
+console.log(myName);
+
+                    //------2--------
+var yourName = prompt("What's your name? ");
+alert("My name is " + myName + ", welcome to my page " + yourName + "!");
+
+                    //------3--------
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your level is currently: " + gameLevel);
