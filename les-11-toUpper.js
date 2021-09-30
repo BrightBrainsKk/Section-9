5ab3c101ebf3755fdd6cb8a4d6d9138802025b3d
Asel Alimbaeva <asel_19.86@mail.ru>
2021-09-30 21:59:55 +0300

    add and write lesson 11

---
+++ les-11-toUpper.js
@@ -0,0 +1,11 @@
+/*
+Метод toUpperCase() возвращает значение строки, на которой он был вызван, преобразованное в верхний регистр.
+Метод toUpperCase() возвращает значение строки, преобразованное в верхний регистр.
+ Метод toUpperCase() не изменяет значение самой строки.
+console.log('алфавит'.toUpperCase()); // 'АЛФАВИТ'
+*/
+
+let as = 'asel';
+document.write(as);
+document.write('<br>' + as.toUpperCase());
+document.write('<br>' + 'anjrla'.toUpperCase());
