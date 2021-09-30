0727362c1a9ff880b5764cba5c5773dc8c75db66
Asel Alimbaeva <asel_19.86@mail.ru>
2021-09-30 22:37:51 +0300

    add and write lesson incriment and decriment

---
+++ incriment.js
@@ -0,0 +1,21 @@
+// Из языка Си в JavaScript перекочевали две операции: инкремент ++ и декремент --,
+//  которые очень часто встречаются вместе с циклами. 
+// Эти унарные операции увеличивают и уменьшают на единицу число, записанное в переменную:
+
+let a, b, c, d;
+b = 2
+document.write('<br>' + b);
+b++
+document.write('<br>' + b);
+a = 3;
+document.write('<br>' + b);
+b += a
+document.write('<br>' + b);
+b *= a
+document.write('<br>' + b);
+b /= a;
+document.write('<br>' + b);
+b--
+document.write('<br>' + b);
+b -= a;
+document.write('<br>' + b);
