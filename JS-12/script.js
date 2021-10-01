127d78b91419c93e90aad15b2fcad6935339edf3
Ainura Bakirova <ainura.bakirova.kk@gmail.com>
2021-10-01 13:37:10 +0600

    12 less

---
+++ JS-12/script.js
@@ -0,0 +1,25 @@
+prompt("What is your name?");
+//1 Create a var that stores the name that user enters via prompt.
+
+var name = prompt("What is your name?");
+
+//2 capitalise the  first letter of their name.
+
+//a Isolate the first char
+ var firstChar = name.slice(0.1);
+
+//e Change the  rest of name to lower case
+var restOfName = restOfName.toLowerCase(1);
+
+//e  concactenate the first char to upper case
+ var upperCaseFirstChar = firstChar.toUpperCase();
+
+// c isolate the rest of the name
+ var restOfName =name.slice(1,name.length);
+
+//d concatenate the first char with the rest of char
+ var capitalisedName = upperCaseFirstChar + requestAnimationFrame;
+
+//3 We use the capitalised version  of  their name to greet them using an alert.
+alert("Hello, " + capitalisedName);
+ Hello, Angela
