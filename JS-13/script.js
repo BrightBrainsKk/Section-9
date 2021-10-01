77a143111337e2558c5f977616c29103306f0691
Ainura Bakirova <ainura.bakirova.kk@gmail.com>
2021-10-01 13:52:21 +0600

    Add 13 les

---
+++ JS-13/script.js
@@ -0,0 +1,21 @@
+//  var a = 2 + 3;//5 Типичная арифметическая операция берет два числа и дает новое число.
+
+//  var b = 10 - 2;// - — вычитание
+
+//  var c = 3 * 3;//9 *  умножение
+
+//  var e = 10 / 2;// / — деление
+
+//  var e = 9 % 6;//3 % — остаток от деления
+
+//  var cost = 3 + 5 * 2; //(8*2=16)
+
+//  var cost = 5 + 5 * 2; //(10*2=20)
+
+// console.log(8 / 2);  // => 4  результат деления
+// console.log(5 + 5 * 2); // => результат 20
+
+
+var dogAge = prompt("How old isyou dog");
+var humanAge = ((dogAge - 2) * 4) +21;
+alert("You dogb is" + humanAge + "yearsold in human years.");
